a04a51cddb7ea8d2d6c7a572041b2990c1a1b50f
johann <johannferrera@outlook.com>
2024-11-10 01:52:17 -0800

    update to remove some old code and to add a refresh button

--- src/screens/MyCalendarScreen.js
+++ src/screens/MyCalendarScreen.js
@@ -1,21 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {
-  View,
-  Text,
-  Button,
-  FlatList,
-  StyleSheet,
-  Alert,
-} from "react-native";
-import { db, auth } from "../firebaseConfig"; // Import your Firestore and Auth configuration
+import { View, Text, Button, FlatList, StyleSheet, Alert } from "react-native";
+import { db, auth } from "../firebaseConfig";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { CalendarSwitcher } from "../components";
 import { Timestamp } from "firebase/firestore";
 
 export default function MyCalendarScreen({ route, navigation }) {
   const [events, setEvents] = useState([]);
-  const userId = auth.currentUser.uid; // Get the currently authenticated user's ID
+  const userId = auth.currentUser.uid;
 
   // State variables for new event
   const [eventTitle, setEventTitle] = useState("");
@@ -23,52 +16,51 @@ export default function MyCalendarScreen({ route, navigation }) {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
+  const fetchEvents = async () => {
+    try {
+      const calendarId = `personal_calendar_${userId}`;
+      const eventsCollection = collection(
+        db,
+        "calendars",
+        calendarId,
+        "events"
+      );
+      const eventsSnapshot = await getDocs(eventsCollection);
+
+      const eventsList = eventsSnapshot.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          id: doc.id,
+          title: data.title,
+          startDate: data.startDate.toDate(),
+          endDate: data.endDate.toDate(),
+          description: data.description,
+        };
+      });
+      setEvents(eventsList);
+    } catch (error) {
+      console.error("Error fetching events: ", error);
+      Alert.alert("Error", "Could not fetch events.");
+    }
+  };
+
   useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const calendarId = `personal_calendar_${userId}`;
-        const eventsCollection = collection(
-          db,
-          "calendars",
-          calendarId,
-          "events"
-        );
-        const eventsSnapshot = await getDocs(eventsCollection);
-
-        const eventsList = eventsSnapshot.docs.map((doc) => {
-          const data = doc.data();
-          return {
-            id: doc.id,
-            title: data.title,
-            startDate: data.startDate.toDate(), // Convert Firestore Timestamp to Date
-            endDate: data.endDate.toDate(), // Convert Firestore Timestamp to Date
-            description: data.description,
-          };
-        });
-        setEvents(eventsList);
-      } catch (error) {
-        console.error("Error fetching events: ", error);
-        Alert.alert("Error", "Could not fetch events.");
-      }
-    };
-
     fetchEvents();
   }, [userId]);
 
   const handleAddEvent = async () => {
     const newEvent = {
       title: eventTitle,
-      startDate: Timestamp.fromDate(startDate), // Use Firestore's Timestamp
-      endDate: Timestamp.fromDate(endDate), // Use Firestore's Timestamp
+      startDate: Timestamp.fromDate(startDate),
+      endDate: Timestamp.fromDate(endDate),
       description: eventDescription,
     };
 
     try {
       const calendarId = `personal_calendar_${userId}`;
       await addDoc(collection(db, "calendars", calendarId, "events"), newEvent);
-      setEvents((prevEvents) => [...prevEvents, newEvent]); // Update local state for immediate UI feedback
+      setEvents((prevEvents) => [...prevEvents, newEvent]);
       Alert.alert("Success", "Event added successfully!");
-      // Reset fields after adding the event
       setEventTitle("");
       setEventDescription("");
       setStartDate(new Date());
@@ -86,6 +78,7 @@ export default function MyCalendarScreen({ route, navigation }) {
         title="Add Event"
         onPress={() => navigation.navigate("AddEvent")}
       />
+      <Button title="Refresh Events" onPress={fetchEvents} />
       <View style={styles.eventsContainer}>
         <FlatList
           data={events}
@@ -108,16 +101,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-  },
-  formContainer: {
-    marginBottom: 16,
-  },
-  input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 10,
-    padding: 10,
   },
   eventsContainer: {
     flex: 1,
