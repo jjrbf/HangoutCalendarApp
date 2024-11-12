c7e6b0da55ca9857738d93c2437d59da13af0c73
johann <johannferrera@outlook.com>
2024-11-12 15:05:37 -0800

    started the code for handling sign up to include a username

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button, Alert, StyleSheet } from "react-native";
 import { auth } from "../firebaseConfig";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-const db = getFirestore(); // Initialize Firestore
+const db = getFirestore();
 
 export default function SignUpScreen({ navigation }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [validUser, setUsernameValid] = useState(false)
+
+  useEffect(() => {
+    async function checkUsername(username) {
+      // code here to determine if the username is valid, make it more than 4 letters and be unique in the database
+      // allow only letters, numbers, and underscores
+      // set validUser to true if valid, set it to false if not
+    }
+    checkUsername(username);
+  }, [username]);
 
   const handleSignUp = async () => {
     // Field validation
-    if (!name || !email || !password) {
+    if (!name || !email || !password || !username) {
       Alert.alert("Validation Error", "Please fill out all fields.");
+      return;
+    }
+    
+    if (!validUser) {
+      Alert.alert("Username taken. Choose a new username.");
       return;
     }
 
@@ -31,6 +47,7 @@ export default function SignUpScreen({ navigation }) {
       // Create user document in the 'users' collection
       await setDoc(doc(db, "users", userId), {
         name: name,
+        username: username,
         email: email,
         profilePicture: "", // or a default picture URL
         friends: [],
@@ -57,6 +74,12 @@ export default function SignUpScreen({ navigation }) {
   return (
     <View style={styles.signInContainer}>
       <Text>Sign Up</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        style={styles.input}
+      />
       <TextInput
         placeholder="Name"
         value={name}
