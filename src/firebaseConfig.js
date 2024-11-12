6e0be3123cf390e991688dd7b5c91889134f9c80
johann <johannferrera@outlook.com>
2024-11-11 16:46:37 -0800

    updated firebaseconfig use config.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,10 +4,16 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, browserLocalPersistence, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from "./config.js";
 
 // Firebase configuration object
 const firebaseConfig = {
-  //*initial*
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 // Initialize Firebase App
